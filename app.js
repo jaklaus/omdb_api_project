fe373e8672bd83c37f520ba0da1d270320af2769
Joel Klaus <jaklaus0101@gmail.com>
2016-09-09 18:42:25 -0500

    initial commit for movie_api_project

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	request = require("request");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get("/", function(req,res){
+
+	if(req.query.search){
+		var query = req.query.search;
+		var url = 'http://www.omdbapi.com/?s=' + query;
+
+		request(url, function(err, response, body){
+			if(!err && response.statusCode === 200){
+				var parsedData = JSON.parse(body)
+				res.render('home', {data:parsedData});
+			}
+		})
+	} else {
+		res.render('home', {data:[]})
+	}
+	
+	
+});
+
+app.get("/movie/:info", function(req,res){
+
+	if(req.params.info){
+		var queryInfo = req.params.info;
+		var url = 'http://www.omdbapi.com/?t=' + queryInfo + "&tomatoes=true";
+		console.log(url)
+		request(url, function(err, response, body){
+			if(!err && response.statusCode === 200){
+				var parsedData = JSON.parse(body)
+				res.render('movie', {data:parsedData});
+			}
+		})
+	} else {
+		res.render('movie', {data:[]})
+	}
+	
+	
+});
+
+app.listen(3000, function(req,res){
+	console.log("movie api server running!")
+});
